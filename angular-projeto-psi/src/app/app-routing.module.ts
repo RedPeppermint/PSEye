3876867c9c633ce8ce8b6e44fbba04beac889d70
jojogilx <jofilipemorais@gmail.com>
2021-04-13 12:01:35 +0100

    Added photos Created routes

--- angular-projeto-psi/src/app/app-routing.module.ts
+++ angular-projeto-psi/src/app/app-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from "@angular/router";
 
+
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { FavoritesComponent } from "./favorites/favorites.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { UploadComponent } from "./upload/upload.component";
+
+
+const routes: Routes = [
+  { path: "dashboard", redirectTo: "", pathMatch: "full" },
+  { path: "", component: DashboardComponent },
+  { path: "favorites", component: FavoritesComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "upload", component: UploadComponent }
+];
 
 
 @NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
   declarations: [],
-  imports: [
-    CommonModule
-  ]
 })
 export class AppRoutingModule { }
