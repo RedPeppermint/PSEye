2c2bbb448714ce87ebe2b54ae38e168f252ad1ed
SaraKmado <56612999+SaraKmado@users.noreply.github.com>
2021-04-13 17:37:42 +0100

    router working, need to make function
    todo: function photo_controller.upload

--- express-projeto-psi/projeto/app.js
+++ express-projeto-psi/projeto/app.js
@@ -9,6 +9,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://psi017:psi017@localhost:27017/psi017?retryWrites=true&authSource=psi017';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -23,12 +29,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- express-projeto-psi/projeto/routes/index.js
+++ express-projeto-psi/projeto/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var photo_controller = require('../controller/photo_controller');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/upload/', photo_controller.upload);
 
 module.exports = router;
