885c97b1c2ddfdd443f4f351186c06b3148ec0be
Eduardo Salvadinha <eduardo.salvadinha@outlook.pt>
2021-04-14 15:36:54 +0100

    REFIXED NODEJS!

--- express-projeto-psi/projeto/controller/photo_controller.js
+++
@@ -1,8 +0,0 @@
-var Photo = require('../models/photo');
-var mongoose = require('mongoose');
-var PhotoSchema = mongoose.model('Photo', Photo.PhotoSchema);
-
-exports.upload = function (req, res) {
-    console.log("TODO function photo_controller: upload");
-    res.send("TODO");
-};
--- express-projeto-psi/projeto/routes/index.js
+++ express-projeto-psi/projeto/routes/index.js
@@ -2,15 +2,7 @@ var express = require('express');
 var router = express.Router();
 var databaseController = require('../controllers/databaseController')
 
+router.get('/init', databaseController.init);
 
-var photo_controller = require('../controller/photo_controller');
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-
-router.post('/upload/', photo_controller.upload);
 
 module.exports = router;
