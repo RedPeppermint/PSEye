3b796829c78e17e279b84f33cc53eb97c517f8eb
Sussurroh <igo.ramalho.atomicbomb@gmail.com>
2021-04-18 15:17:58 +0100

    s adicionado

--- express-projeto-psi/projeto/app.js
+++ express-projeto-psi/projeto/app.js
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(expres.json({ limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
